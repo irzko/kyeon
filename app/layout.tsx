96e6d1b58bfdfd5092092ade869e8e615c96b5c1
irzko <tranminhkha.1003u@gmail.com>
2023-08-20 15:57:39 +0700

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="vi">
       <body
         className={clsx(
           inter.className,
