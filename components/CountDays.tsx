cbd91ae53570e2affcb2c84b3c386787c7027493
irzko <tranminhkha.1003u@gmail.com>
2023-08-22 18:09:39 +0700

    .

--- components/CountDays.tsx
+++ components/CountDays.tsx
@@ -7,49 +7,49 @@ const CountDays = () => {
   // const [hours, setHours] = useState(0);
   // const [minutes, setMinutes] = useState(0);
   // const [seconds, setSeconds] = useState(0);
-  const [love, setLove] = useState(0);
+  // const [love, setLove] = useState(0);
 
   const loveDay = "July, 27, 2023";
 
-  useEffect(() => {
-    fetch("/api/love")
-      .then((res) => res.json())
-      .then((data) => {
-        fetch("/api/love", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            love: data + 1,
-          }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            setLove(data);
-          });
-      });
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/love")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       fetch("/api/love", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({
+  //           love: data + 1,
+  //         }),
+  //       })
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //           setLove(data);
+  //         });
+  //     });
+  // }, []);
 
-  const handleClick = () => {
-    fetch("/api/love")
-      .then((res) => res.json())
-      .then((data) => {
-        fetch("/api/love", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            love: data + 1,
-          }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            setLove(data);
-          });
-      });
-  };
+  // const handleClick = () => {
+  //   fetch("/api/love")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       fetch("/api/love", {
+  //         method: "POST",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //         body: JSON.stringify({
+  //           love: data + 1,
+  //         }),
+  //       })
+  //         .then((res) => res.json())
+  //         .then((data) => {
+  //           setLove(data);
+  //         });
+  //     });
+  // };
 
   const getTime = (loveDay: string) => {
     const time = Date.now() - Date.parse(loveDay);
@@ -68,15 +68,15 @@ const CountDays = () => {
   return (
     <>
       <div className="absolute">
-        {love ? (
+        {days ? (
           <div
             className="flex flex-col justify-center items-center"
-            onClick={handleClick}
+            // onClick={handleClick}
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width={280}
-              fill="#FCBAAD"
+              fill="#E74646"
               className="blur-2xl"
               viewBox="0 0 16 16"
             >
@@ -88,7 +88,7 @@ const CountDays = () => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width={280}
-              fill="#FCBAAD"
+              fill="#E74646"
               className="absolute"
               viewBox="0 0 16 16"
             >
@@ -99,17 +99,17 @@ const CountDays = () => {
             </svg>
             {/* <Image src={Heart} width={500} height={500} className=" absolute mt-12" alt="" quality={100} /> */}
             <div className="z-10 absolute mb-10 flex flex-col items-center">
-              <h2 className="text-[#A2678A] flex items-center text-xl font-bold">
+              <h2 className="text-white flex items-center text-xl font-bold">
                 Nguyệt
-                <p className="text-[#BB2525] mx-2">đã bên</p> Kha
+                <p className="text-[#C21010] mx-2">đã bên</p> Kha
               </h2>
 
               <div className="text-5xl font-bold text-white">
-                <span className="text-[#BB2525]">{days}</span> ngày
+                <span className="text-[#C21010]">{days}</span> ngày
               </div>
-              <p className="text-[#BB2525] mt-3 font-bold px-4 bg-white rounded-full">
+              {/* <p className="text-[#BB2525] mt-3 font-bold px-4 bg-white rounded-full">
                 Thương x{love} lần
-              </p>
+              </p> */}
             </div>
             {/* <p>
             {hours} giờ {minutes} phút {seconds} giây
