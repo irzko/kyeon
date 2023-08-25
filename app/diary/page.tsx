80cd4917f98eaac290e43a58ac4cf78330d5c325
irzko <tranminhkha.1003u@gmail.com>
2023-08-26 01:37:17 +0700

    .

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -1,37 +1,12 @@
+import Diary from "@/components/diary";
 import getApiUrl from "@/libs/api-url";
 import DiaryType from "@/types";
 import Link from "next/link";
-import Dropdown from "@/components/Dropdown";
 
 async function getDiaries(): Promise<DiaryType[]> {
   const res = await fetch(`${getApiUrl()}/api/diary/`, { cache: "no-store" });
   return res.json();
 }
-
-
-const Diary = ({ diary }: { diary: DiaryType }) => {
-  return (
-    <li className="mb-4 ml-4">
-      <div className="absolute w-3 h-3 bg-[#7F669D] rounded-full mt-4 -left-1.5 border border-white"></div>
-      <div className="relative px-2 py-2 bg-white rounded-3xl">
-        <div className="flex justify-between">
-          <time className="mb-1 text-sm leading-none font-bold text-[#7F669D] bg-[#F8E8EE] rounded-full px-2 flex items-center">
-            {new Date(diary.datetime).toLocaleString()}
-          </time>
-          <Dropdown diary={diary} />
-        </div>
-        <div className="p-4">
-          <h3 className="text-lg font-semibold text-gray-900">
-            {diary.content}
-          </h3>
-          <p className="text-base font-normal italic text-gray-500 text-right">
-            {diary.author}
-          </p>
-        </div>
-      </div>
-    </li>
-  );
-};
 
 export default async function Page() {
   const diaries = await getDiaries();
