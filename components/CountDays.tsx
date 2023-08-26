dcb004970d7596aa0030229e90cb2c76dd378dcc
irzko <tranminhkha.1003u@gmail.com>
2023-08-26 17:12:32 +0700

    .

--- components/CountDays.tsx
+++ components/CountDays.tsx
@@ -1,9 +1,17 @@
 "use client";
 import { useState, useEffect } from "react";
-import Spinner from "./Spinner";
 import Heart from "./Heart";
 import LoadingDots from "./loading-dots";
 import { useRouter } from "next/navigation";
+import localFont from 'next/font/local'
+
+const rubraCosta = localFont({
+  src: './fonts/DFVN Rubra Costa.otf'
+})
+
+const indulgeScript = localFont({
+  src: './fonts/DFVN Indulge Script.otf'
+});
 
 const CountDays = () => {
   const [days, setDays] = useState(0);
@@ -41,22 +49,16 @@ const CountDays = () => {
             <Heart />
             <div className="z-10 absolute mb-10 flex flex-col items-center">
               <h2
-                className={`text-[#A084E8] flex items-center text-xl z-10 uppercase`}
+                className={`text-[#A084E8] font-medium flex items-center text-xl z-10 uppercase`}
               >
                 Nguyệt
                 <p className="text-[#8BE8E5] mx-2">x</p> Kha
               </h2>
 
-              <div className={`text-4xl font-bold text-[#A084E8] uppercase`}>
-                <span className="text-[#8BE8E5]">{days}</span> days
+              <div className={`text-6xl font-bold text-[#A084E8] ${rubraCosta.className}`}>
+                <span className="text-[#8BE8E5]">{days}</span> Ngày
               </div>
-              {/* <p className="text-[#BB2525] mt-3 font-bold px-4 bg-white rounded-full">
-                Thương x{love} lần
-              </p> */}
             </div>
-            {/* <p>
-            {hours} giờ {minutes} phút {seconds} giây
-          </p> */}
           </div>
         ) : (
           <LoadingDots />
