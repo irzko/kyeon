c26c9b5057f8a1e092f5f6414c4e46a6bbb8734c
irzko <tranminhkha.1003u@gmail.com>
2023-10-11 14:24:14 +0700

    .

--- components/diary-card.tsx
+++ components/diary-card.tsx
@@ -1,13 +1,14 @@
 import Dropdown from "@/components/Dropdown";
 
 const DiaryCard = ({ diary }: { diary: IDiary }) => {
+  const date = new Date(diary.date).toLocaleString("vi-VN");
   return (
     <li className="mb-4 ml-4">
       <div className="absolute w-3 h-3 bg-blue-100 dark:bg-blue-900 rounded-full mt-5 -left-1.5 ring-1 ring-white dark:ring-gray-900"></div>
       <div className="flex flex-col bg-white rounded-2xl border border-gray-100 hover:border-white dark:border-gray-800 dark:hover:border-gray-700 hover:shadow-lg dark:hover:shadow-lg-light dark:bg-gray-900">
         <div className="flex justify-between pt-2 px-2">
           <time className="bg-blue-100 flex items-center font-bold text-blue-800 text-sm mr-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300">
-            {new Date(diary.date).toLocaleString("vi-VN")}
+            {date}
           </time>
           <Dropdown diary={diary} />
         </div>
