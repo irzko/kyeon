2e2ea658673eadc911b45d5713d4bc060e76a69a
irzko <tranminhkha.1003u@gmail.com>
2023-10-11 07:31:01 +0700

    .

--- app/diary/loading.tsx
+++ app/diary/loading.tsx
@@ -2,7 +2,7 @@ import Spinner from "@/components/Spinner";
 
 const Loading = () => {
   return (
-    <div className="fixed w-full h-full top-0 left-0 flex justify-center items-center">
+    <div className="fixed w-full h-full top-14 left-0 flex justify-center items-center">
       <Spinner height={32} width={32} fill="#2563eb" />
     </div>
   );
