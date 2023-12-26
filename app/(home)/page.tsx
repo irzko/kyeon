c91f41a7a5ff6604d0fa75da221bf72ed28cf49a
irzko <tranminhkha.1003u@gmail.com>
2023-12-26 09:43:44 +0700

    Remove XmasTheme and add Background and CountDays components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,15 @@
-import XmasTheme from "@/components/xmas-theme";
+// import XmasTheme from "@/components/xmas-theme";
+
+import Background from "@/components/background";
+import CountDays from "@/components/count-days";
 
 export default function Page() {
   return (
     <>
       <div className="z-50 flex overflow-hidden place-items-center min-h-screen m-0 justify-center items-center flex-col">
-        <XmasTheme />
+        {/* <XmasTheme /> */}
+        <Background />
+        <CountDays />
       </div>
     </>
   );
