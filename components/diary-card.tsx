2a44e0f39d032840f9233437ffac9ee21ba9886f
Kha <53520595+irzko@users.noreply.github.com>
2024-11-10 19:29:24 +0700

    Update diary-card.tsx

--- components/diary-card.tsx
+++ components/diary-card.tsx
@@ -104,20 +104,6 @@ const ActionMenu = ({ diary }: { diary: IDiary }) => {
   );
 };
 
-const cardVariants: Variants = {
-  offscreen: {
-    y: 250
-  },
-  onscreen: {
-    y: 0,
-    transition: {
-      type: "spring",
-      bounce: 0.4,
-      duration: 0.8
-    }
-  }
-};
-
 const DiaryCard = ({ diary }: { diary: IDiary }) => {
   return (
     <>
