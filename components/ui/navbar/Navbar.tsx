a18f2cc6d7f252db4012ec63c8a7e800787571f1
Kha <53520595+irzko@users.noreply.github.com>
2024-11-10 15:51:38 +0700

    Update Navbar.tsx

--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -2,7 +2,7 @@ export default function Navbar({
   children,
 }: Readonly<{ children?: React.ReactNode }>) {
   return (
-    <nav className="flex z-40 w-full h-auto items-center justify-center sticky top-0 inset-x-0 backdrop-blur-lg backdrop-saturate-150 border-b border-b-gray-200 bg-white/70">
+    <nav className="flex z-40 w-full h-auto items-center justify-center sticky top-0 inset-x-0 backdrop-blur-lg backdrop-saturate-150 border-b border-b-gray-600 bg-gray-900/70">
       <header className="z-40 flex px-6 gap-4 w-full flex-row relative flex-nowrap items-center justify-between h-16 max-w-screen-lg">
         {children}
       </header>
