05fb981aa1f095f11e05a427ed39333f38900154
Kha <53520595+irzko@users.noreply.github.com>
2024-11-10 15:53:36 +0700

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,15 +1,15 @@
-import XmasTheme from "@/components/xmas-theme";
+// import XmasTheme from "@/components/xmas-theme";
 
-// import Background from "@/components/background";
-// import CountDaysPage from "@/components/count-days";
+import Background from "@/components/background";
+import CountDaysPage from "@/components/count-days";
 
 export default function Page() {
   return (
     <>
       <div className="flex fixed overflow-hidden place-items-center inset-0 m-0 justify-center items-center flex-col">
-        <XmasTheme />
-        {/* <CountDaysPage /> 
-        <Background /> */}
+        {/* <XmasTheme /> */}
+        <CountDaysPage /> 
+        <Background />
       </div>
     </>
   );
