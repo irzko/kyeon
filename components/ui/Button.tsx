f029ce417b3e7d15df5c8c9e6a580da622573842
Kha <53520595+irzko@users.noreply.github.com>
2024-11-11 22:36:49 +0700

    Update Button.tsx

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -6,13 +6,13 @@ const buttonStyles = tv({
   variants: {
     color: {
       default:
-        "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800",
+        "text-white focus:ring-4 bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-blue-800",
       light:
-        "text-gray-900 bg-white focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700",
+        "focus:outline-none focus:ring-4 bg-gray-800 text-white border-gray-600 hover:bg-gray-700 hover:border-gray-600 focus:ring-gray-700",
       danger:
-        "text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900",
+        "focus:outline-none focus:ring-4 bg-red-600 text-white hover:bg-red-700 focus:ring-red-900",
       dark:
-        "text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+        "text-white focus:outline-none focus:ring-4 bg-gray-800 hover:bg-gray-700 focus:ring-gray-700 border-gray-700"
     },
 
     radius: {
