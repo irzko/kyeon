2d62c4da24faed940bc35d5392a96f86c64e254c
Kha <53520595+irzko@users.noreply.github.com>
2024-11-11 23:05:29 +0700

    Update Input.tsx

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -2,7 +2,7 @@ import { DetailedHTMLProps, InputHTMLAttributes } from "react";
 import { tv } from "tailwind-variants";
 
 const inputStyles = tv({
-  base: "bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
+  base: "border text-sm rounded-lg outline-none focus:ring-1 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500",
 });
 
 export default function Input(
