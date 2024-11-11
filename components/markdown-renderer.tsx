eae153a05e5185cd7a79cc873efb8febfb74ec03
Kha <53520595+irzko@users.noreply.github.com>
2024-11-11 19:07:54 +0700

    Delete components/markdown-renderer.tsx

--- components/markdown-renderer.tsx
+++
@@ -1,8 +0,0 @@
-import Markdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-
-export default function MarkdownRenderer ({ content } : {content : string}) {
-  return (
-    <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
-  );
-}
