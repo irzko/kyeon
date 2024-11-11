a80133afee952946f1be6768f95313000e6d3f09
Kha <53520595+irzko@users.noreply.github.com>
2024-11-12 00:34:15 +0700

    Update page.tsx

--- app/diary/edit/[diaryId]/page.tsx
+++ app/diary/edit/[diaryId]/page.tsx
@@ -103,7 +103,7 @@ const Page = async ({ params }: { params: Promise<{ diaryId: string }> }) => {
             id="content"
             rows={15}
             name="content"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg outline-none focus:ring-1 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="border text-sm rounded-lg outline-none focus:ring-1 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
             placeholder="Hãy viết gì đó..."
             defaultValue={diary?.content}
             required
