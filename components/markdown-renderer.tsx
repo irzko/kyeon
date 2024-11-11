fe1c02a883e6ba04012e789cefee6a235089185f
Kha <53520595+irzko@users.noreply.github.com>
2024-11-11 19:00:03 +0700

    Update markdown-renderer.tsx

--- components/markdown-renderer.tsx
+++ components/markdown-renderer.tsx
@@ -1,1 +1,8 @@
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 
+export default function MarkdownRenderer ({ content } : {content : string}) {
+  return (
+    <Markdown remarkPlugins={[remarkGfm]}>{content}</Markdown>
+  );
+}
