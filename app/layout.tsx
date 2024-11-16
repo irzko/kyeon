b172d0544a82ba7b93c655dd0a096c38a89f4c38
Kha <53520595+irzko@users.noreply.github.com>
2024-11-16 12:14:00 +0000

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import "moment/locale/vi";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Đếm ngày chúng ta bên nhau",
@@ -17,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="vi">
-      <body className={`bg-gray-900 ${inter.className}`}>{children}</body>
+      <body className={`bg-gray-900 font-sans`}>{children}</body>
     </html>
   );
 }
