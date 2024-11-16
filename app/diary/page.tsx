a76f7c7f6479eeea44876d33342acac9d32d46cf
Kha <53520595+irzko@users.noreply.github.com>
2024-11-16 18:35:08 +0000

    Update

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -23,12 +23,12 @@ export default async function Page() {
         <NavbarContent>
           <NavbarItem>
             <ButtonLink
+              radius="lg"
               className="gap-2 font-bold"
-              color="default"
-              isIconOnly
+              color="dark"
               href="/diary/create"
             >
-              <svg className="w-[36px] h-[36px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <svg className="w-[36px] h-[36px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" />
               </svg>
             </ButtonLink>
--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -1,11 +1,11 @@
 import { tv } from "tailwind-variants";
 
 const navbar = tv({
-  base: "flex z-40 w-full h-auto items-center justify-center sticky inset-x-0 backdrop-blur-lg backdrop-saturate-150 bg-gray-900/70",
+  base: "flex z-40 w-full h-auto items-center justify-center inset-x-0 backdrop-blur-lg backdrop-saturate-150 bg-gray-900/70",
   variants: {
     position: {
-      top: "top-0 border-b border-b-gray-600",
-      bottom: "bottom-0 border-t border-t-gray-600",
+      top: "top-0 sticky border-b border-b-gray-600",
+      bottom: "fixed bottom-0 border-t border-t-gray-600",
     }
   }
 });
