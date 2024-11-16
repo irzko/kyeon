8ca53b9161a9e72272cd89da0a391628388c57f1
Kha <53520595+irzko@users.noreply.github.com>
2024-11-16 18:21:04 +0000

    Update navbar

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -1,13 +1,10 @@
-import Link from "next/link";
 import { unstable_cache } from 'next/cache';
 import prisma from "@/libs/prisma";
-import { Style_Script } from "next/font/google";
 import { Navbar, NavbarContent, NavbarItem } from "@/components/ui/navbar";
-import NavbarBrand from "@/components/ui/navbar/NavbarBrand";
+
 import Post from "@/components/diary/post";
 import ButtonLink from "@/components/ui/ButtonLink";
 
-const styleScript = Style_Script({ subsets: ["vietnamese"], weight: ["400"] });
 
 const getPosts = unstable_cache(
   async () => {
@@ -21,44 +18,19 @@ export default async function Page() {
   const posts: IDiary[] = await getPosts();
   return (
     <div className="graph-paper">
-      <Navbar>
-        <NavbarContent>
-          <NavbarBrand>
-            <Link
-              href="/"
-              className={` ${styleScript.className} self-center text-3xl font-black whitespace-nowrap text-white`}
-            >
-              #nhatkyvutru
-            </Link>
-          </NavbarBrand>
-        </NavbarContent>
+      <Navbar position="bottom">
 
         <NavbarContent>
           <NavbarItem>
             <ButtonLink
-              radius="full"
               className="gap-2 font-bold"
               color="default"
+              isIconOnly
               href="/diary/create"
             >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 12h14m-7 7V5"
-                />
+              <svg className="w-[36px] h-[36px] text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" />
               </svg>
-              Tạo
             </ButtonLink>
           </NavbarItem>
         </NavbarContent>
--- components/ui/navbar/Navbar.tsx
+++ components/ui/navbar/Navbar.tsx
@@ -1,8 +1,26 @@
+import { tv } from "tailwind-variants";
+
+const navbar = tv({
+  base: "flex z-40 w-full h-auto items-center justify-center sticky inset-x-0 backdrop-blur-lg backdrop-saturate-150 bg-gray-900/70",
+  variants: {
+    position: {
+      top: "top-0 border-b border-b-gray-600",
+      bottom: "bottom-0 border-t border-t-gray-600",
+    }
+  }
+});
+
 export default function Navbar({
   children,
-}: Readonly<{ children?: React.ReactNode }>) {
+  position = "top",
+}: Readonly<{
+  children?: React.ReactNode;
+  position?: "top" | "bottom";
+}>) {
   return (
-    <nav className="flex z-40 w-full h-auto items-center justify-center sticky top-0 inset-x-0 backdrop-blur-lg backdrop-saturate-150 border-b border-b-gray-600 bg-gray-900/70">
+    <nav className={navbar({
+position
+    })}>
       <header className="z-40 flex px-6 gap-4 w-full flex-row relative flex-nowrap items-center justify-between h-16 max-w-screen-lg">
         {children}
       </header>
