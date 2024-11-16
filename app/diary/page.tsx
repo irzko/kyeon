e29ef707f24efbf25f5978656d7f061adaecc6da
Kha <53520595+irzko@users.noreply.github.com>
2024-11-16 19:56:42 +0000

    Update

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -24,6 +24,12 @@ export default async function Page() {
 
         <NavbarContent className="w-full">
           <NavbarItem>
+            <Link href="/diary">
+              <svg className="w-[36px] h-[36px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m4 12 8-8 8 8M6 10.5V19a1 1 0 0 0 1 1h3v-3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3h3a1 1 0 0 0 1-1v-8.5" />
+              </svg></Link>
+          </NavbarItem>
+          <NavbarItem>
             <ButtonLink
               radius="full"
               isIconOnly
@@ -37,16 +43,23 @@ export default async function Page() {
               </svg>
             </ButtonLink>
           </NavbarItem>
+          <NavbarItem>
+            <Link href="/">
+              <svg className="w-[36px] h-[36px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z" />
+              </svg>
+            </Link>
+          </NavbarItem>
         </NavbarContent>
       </Navbar>
 
       <main className="max-w-screen-sm mx-auto p-4">
         <Link
-              href="/"
-              className={` ${styleScript.className} mb-4 block text-center text-3xl font-black whitespace-nowrap text-white`}
-            >
-              #nhatkyvutru
-            </Link>
+          href="/"
+          className={` ${styleScript.className} mb-4 block text-center text-3xl font-black whitespace-nowrap text-white`}
+        >
+          #nhatkyvutru
+        </Link>
         <ul className="mb-20 list-none space-y-4">
           {posts?.map((post) => (
             <Post key={post.id} diary={post} />
