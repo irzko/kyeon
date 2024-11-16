dd969749fa691139718a76547db69d5c6eb307e5
Kha <53520595+irzko@users.noreply.github.com>
2024-11-16 20:17:08 +0000

    Fix

--- app/diary/page.tsx
+++ app/diary/page.tsx
@@ -36,7 +36,7 @@ export default async function Page() {
               <svg className="w-[36px] h-[36px] text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M5 12h14m-7 7V5" />
               </svg>
-            </ButtonLink>
+            </Link>
           </NavbarItem>
           <NavbarItem>
             <Link href="/">
