726f5575056c1c0775cb593dc892f83f1517d387
Kha <53520595+irzko@users.noreply.github.com>
2024-11-17 00:13:30 +0000

    Update

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -52,8 +52,8 @@ export default function SearchPage() {
             <NavbarItem>
               <Link href="/search">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={28} height={28} color={"#ffffff"} fill={"none"}>
-                  <path d="M17.5 17.5L22 22" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                  <path d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
+                  <path d="M17.5 17.5L22 22" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+                  <path d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z" stroke="currentColor" strokeWidth="2.5" strokeLinejoin="round" />
                 </svg>
               </Link>
             </NavbarItem>
