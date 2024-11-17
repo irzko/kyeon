4e6c9149925e000eecd77e3404c119cba5116e6c
Kha <53520595+irzko@users.noreply.github.com>
2024-11-17 14:00:39 +0000

    Update

--- app/diary/edit/[diaryId]/page.tsx
+++ app/diary/edit/[diaryId]/page.tsx
@@ -58,7 +58,7 @@ const Page = async ({ params }: { params: Promise<{ diaryId: string }> }) => {
         <NavbarContent>
           <NavbarItem className="flex items-center gap-2">
             <ButtonLink color="dark" className="bg-transparent" isIconOnly href="/diary">
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={28} height={28} color={"#ffffff"} fill={"none"}>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={24} height={24} color={"#ffffff"} fill={"none"}>
                 <path d="M19.0005 4.99988L5.00049 18.9999M5.00049 4.99988L19.0005 18.9999" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
               </svg>
             </ButtonLink>
--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -26,8 +26,7 @@ export default function SearchPage() {
               <Link href="/search">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width={28} height={28} color={"#ffffff"} fill={"none"}>
                   <path d="M17.5 17.5L22 22" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                  <path d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z" stroke="currentColor" strokeWidth="2
-                    1.5" strokeLinejoin="round" />
+                  <path d="M20 11C20 6.02944 15.9706 2 11 2C6.02944 2 2 6.02944 2 11C2 15.9706 6.02944 20 11 20C15.9706 20 20 15.9706 20 11Z" stroke="currentColor" strokeWidth="1.5" strokeLinejoin="round" />
                 </svg>
               </Link>
             </NavbarItem>
