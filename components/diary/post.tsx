6558513de9e5e217a13b07fce6607f638edcd5aa
Kha <53520595+irzko@users.noreply.github.com>
2024-11-17 00:16:27 +0000

    Fix bugs

--- components/diary/post.tsx
+++ components/diary/post.tsx
@@ -22,7 +22,7 @@ const Post = ({ diary }: { diary: IDiary }) => {
             <ActionMenu diary={diary} />
           </div>
           <div className="py-6 px-4 space-y-6">
-            <Markdown remarkPlugins={[[remarkGfm, { singleTilde: false }], [emoji, { emoticon: true }], [supersub], [remarkIns]}>{diary.content}</Markdown>
+            <Markdown remarkPlugins={[[remarkGfm, { singleTilde: false }], [emoji, { emoticon: true }], [supersub], [remarkIns]]}>{diary.content}</Markdown>
             <p className="text-base font-normal text-gray-400 text-center">
               by <strong>{diary.author}</strong>
             </p>
